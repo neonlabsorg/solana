c5322e08cefa4174cbf90d35f0630cbdc463dc39
Ivan Loboda <loboda.ivan.y@gmail.com>
2022-08-02 13:12:00 +0400

    add bank manager

--- neon-tracer/src/main.rs
+++ neon-tracer/src/main.rs
@@ -4,6 +4,16 @@ use {
 use solana_runtime::bank::BankRc;
 use solana_runtime::accounts::Accounts;
 
+pub struct BanksManager {
+    banks: Map<Slot, Bank>,
+}
+
+impl BanksManager {
+    pub fn get_or_create_bank(&mut self, slot: Slot) -> Option<&Bank> {
+
+    }
+}
+
 pub fn main() {
     let slot = 0;
     let accounts = Accounts::new_for_tracer();
