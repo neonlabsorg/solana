4463aa5dfe0ef644162a2231c4bb974826ecb177
Ivan Loboda <loboda.ivan.y@gmail.com>
2022-10-19 14:29:22 +0400

    switch to evm v0.12.1

--- .buildkite/steps/revision.sh
+++ .buildkite/steps/revision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set ${NEON_EVM_REVISION:=ci-develop-neon-tracer}
+set ${NEON_EVM_REVISION:=ci-v0.12.1-neon-tracer}
 
 echo "Neon Validator revision=${BUILDKITE_COMMIT}"
 echo "Neon EVM revision=${NEON_EVM_REVISION}"
