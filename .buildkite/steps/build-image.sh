fa1483fff9c1a11c9b8022eaf18d658cb86538d2
Ivan Loboda <loboda.ivan.y@gmail.com>
2022-11-08 20:41:57 +0400

    add plugin files

---
+++ .buildkite/steps/build-image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+source .buildkite/steps/revision.sh
+
+echo -e "\n\n\nBuilding Neon Validator..."
+docker build -t neonlabsorg/neon-validator:${BUILDKITE_COMMIT} \
+  --build-arg NEON_EVM_REVISION=${NEON_EVM_REVISION} \
+  --build-arg NEON_TRACER_REVISION=${BUILDKITE_COMMIT} .
+
+echo -r "\n\n\nBuilding Accounts DB..."
+docker build -t neonlabsorg/neon-accountsdb:${BUILDKITE_COMMIT} ./neon-dumper-plugin/db
