37f44d876184ba8d2571940f4b5771504d97cb87
Ivan Loboda <loboda.ivan.y@gmail.com>
2023-01-03 16:50:43 +0400

    Take latest EVM version

--- .buildkite/steps/revision.sh
+++ .buildkite/steps/revision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set ${NEON_EVM_REVISION:=ci-v0.14.0-neon-tracer}
+set ${NEON_EVM_REVISION:=ci-develop-neon-tracer}
 
 echo "Neon Validator revision=${BUILDKITE_COMMIT}"
 echo "Neon EVM revision=${NEON_EVM_REVISION}"
