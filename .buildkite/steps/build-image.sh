2a2cdc85b367735d08cfed1fe1ff11909c7a7ba4
Ivan Loboda <loboda.ivan.y@gmail.com>
2023-01-04 20:14:21 +0400

    Pull evm_loader before starting build

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -2,6 +2,8 @@
 set -euo pipefail
 source .buildkite/steps/revision.sh
 
+docker pull neonlabsorg/evm_loader:${NEON_EVM_REVISION}
+
 echo -e "\n\n\nBuilding Neon Validator..."
 docker build -t neonlabsorg/neon-validator:${BUILDKITE_COMMIT} \
   --build-arg NEON_EVM_REVISION=${NEON_EVM_REVISION} \
