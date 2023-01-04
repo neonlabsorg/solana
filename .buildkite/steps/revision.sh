908c52f1e7e8c750d8731b06dc33fbf929fcf59a
Ivan Loboda <loboda.ivan.y@gmail.com>
2023-01-04 12:55:39 +0400

    Set specific EVM revision

--- .buildkite/steps/revision.sh
+++ .buildkite/steps/revision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set ${NEON_EVM_REVISION:=ci-develop-neon-tracer}
+set ${NEON_EVM_REVISION:=e1671c0e1709cff1ee75eff499c3e0311c4cb665}
 
 echo "Neon Validator revision=${BUILDKITE_COMMIT}"
 echo "Neon EVM revision=${NEON_EVM_REVISION}"
