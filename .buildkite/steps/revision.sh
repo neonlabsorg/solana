4f3fda7c1b6d51dacfb34a947e69a14b7701fdce
sinev-valentine <sinev-valentine@yandex.ru>
2023-01-23 12:32:44 +0300

    set NEON_EVM_REVISION=088d0cd66dcb398b09236eb54605ac06ad773c19

--- .buildkite/steps/revision.sh
+++ .buildkite/steps/revision.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-set ${NEON_EVM_REVISION:=latest}
+#set ${NEON_EVM_REVISION:=latest}
+set ${NEON_EVM_REVISION:=088d0cd66dcb398b09236eb54605ac06ad773c19}
 
 echo "Neon Validator revision=${BUILDKITE_COMMIT}"
 echo "Neon EVM revision=${NEON_EVM_REVISION}"
