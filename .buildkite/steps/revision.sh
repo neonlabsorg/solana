65a743720761c5684631166c4073e4bdc8104101
Ivan Loboda <loboda.ivan.y@gmail.com>
2023-02-08 17:24:28 +0400

    use evm_loader build from NDEV-1301

--- .buildkite/steps/revision.sh
+++ .buildkite/steps/revision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set ${NEON_EVM_REVISION:=latest}
+set ${NEON_EVM_REVISION:=32bf97c246d9baf4a678e658e4b62b65d120100a}
 
 echo "Neon Validator revision=${BUILDKITE_COMMIT}"
 echo "Neon EVM revision=${NEON_EVM_REVISION}"
