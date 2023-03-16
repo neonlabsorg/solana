82a9688d5656e4e5d631b902cd5f17a112d401c6
Ivan Loboda <loboda.ivan.y@gmail.com>
2023-03-16 15:29:42 +0400

    Revert "Use Neon-EVM v0.15.3"
    This reverts commit d22dbd048904675d6218d50cd71cbbfb1282d6dd.

--- .buildkite/steps/revision.sh
+++ .buildkite/steps/revision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set ${NEON_EVM_REVISION:=v0.15.3}
+set ${NEON_EVM_REVISION:=a0c93d341d278d08d4f9505b01087b9f89a12b44}
 
 echo "Neon Validator revision=${BUILDKITE_COMMIT}"
 echo "Neon EVM revision=${NEON_EVM_REVISION}"
