e42fba187f1f8dfb980992277ee3ae7554c680ab
Ivan Loboda <loboda.ivan.y@gmail.com>
2023-03-16 22:13:16 +0400

    Use fixed build of EVM

--- .buildkite/steps/revision.sh
+++ .buildkite/steps/revision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set ${NEON_EVM_REVISION:=latest}
+set ${NEON_EVM_REVISION:=5a4fa77fd32d25022b6fa51a7651e2bced3d09a0}
 
 echo "Neon Validator revision=${BUILDKITE_COMMIT}"
 echo "Neon EVM revision=${NEON_EVM_REVISION}"
