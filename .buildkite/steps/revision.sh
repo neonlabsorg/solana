5714a6c94007f0faec5463b86642c4cc9c5f16ab
Ivan Loboda <loboda.ivan.y@gmail.com>
2023-03-18 13:13:08 +0400

    Use specific build of EVM from develop

--- .buildkite/steps/revision.sh
+++ .buildkite/steps/revision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set ${NEON_EVM_REVISION:=5a4fa77fd32d25022b6fa51a7651e2bced3d09a0}
+set ${NEON_EVM_REVISION:=72f95822fe1388adf0d8cbd96c0261188c0510dd}
 
 echo "Neon Validator revision=${BUILDKITE_COMMIT}"
 echo "Neon EVM revision=${NEON_EVM_REVISION}"
