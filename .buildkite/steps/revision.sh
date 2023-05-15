abc48dfd2872e71115b76f8046648049bfcf5417
Denis K <100715772+dk-neon@users.noreply.github.com>
2023-05-15 14:28:08 +0500

    Updated Neon EVM revision

--- .buildkite/steps/revision.sh
+++ .buildkite/steps/revision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set ${NEON_EVM_REVISION:=58b112166d92a02c511a843df0fe6209d03eb024}
+set ${NEON_EVM_REVISION:=a482f6467c5898eab786ffa03b1ae75dcd981d3c}
 
 echo "Neon Validator revision=${BUILDKITE_COMMIT}"
 echo "Neon EVM revision=${NEON_EVM_REVISION}"
